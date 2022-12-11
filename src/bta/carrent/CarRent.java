f71c9d8cbbf7a6a9d747c423f64b1545ad2de015
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-11 18:07:44 +0100

    Remove logger field and setMap method

--- src/bta/carrent/CarRent.java
+++ src/bta/carrent/CarRent.java
@@ -5,12 +5,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
 import java.util.UUID;
-import java.util.logging.Logger;
 
 public class CarRent extends JavaPlugin {
 
     public static String pluginName = "[CarRent] ";
-    public Logger logger = getLogger();
 
     private HashMap<UUID, EingabeKlasse> map = new HashMap<>();
 
@@ -18,12 +16,8 @@ public class CarRent extends JavaPlugin {
         return map;
     }
 
-    public void setMap(HashMap<UUID, EingabeKlasse> map) {
-        this.map = map;
-    }
-
     @Override
     public void onEnable() {
-        logger.info("Plugin wurde aktiviert.");
+        getLogger().info("Plugin wurde aktiviert.");
     }
 }
